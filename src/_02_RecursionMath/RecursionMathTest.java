098f03fd4ec8ee1908ae48107262f8c062dbc07a
league <bobby>
2025-11-01 12:56:40 -0700

    end of level3

--- src/_02_RecursionMath/RecursionMathTest.java
+++ src/_02_RecursionMath/RecursionMathTest.java
@@ -10,7 +10,7 @@ class RecursionMathTest {
     @Test
     public void testMultiplication() {
         assertEquals(12, RecursionMath.recursiveMultiplication(3, 4));
-        
+      
         // Add more JUnit tests like the one above to test your method
         
     }
@@ -18,12 +18,15 @@ class RecursionMathTest {
     @Test
     public void testDivision() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(3, RecursionMath.recursiveDivision(10, 3));
+        assertEquals(2, RecursionMath.recursiveDivision(12, 6));
     }
 
     @Test 
     public void testPower() {
         // Add JUnit tests to test your method
-        fail("Write some tests!");
+        assertEquals(4, RecursionMath.recursivePower(2, 2));
+        assertEquals(27, RecursionMath.recursivePower(3, 3));
+        assertEquals(125, RecursionMath.recursivePower(5, 3));
     }
 }
